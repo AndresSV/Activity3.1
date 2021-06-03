2a057db1ff934ece819e3ce851f42df648a89f04
Andres Salas Villarreal <andres-salas@hotmail.com>
2021-06-02 21:02:14 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,56 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, ScrollView, Alert } from 'react-native';
 
 const Friends = (props: any) => {
-  const [item, setItem] = useState(props);
   return (
-    <TouchableOpacity style={styles.item} onPress={()=> {
-
-    }}>
-      <View style={styles.item}>
-        <Text>{'\u2022'} Name: {props.name}, Hobby: {props.hobby} </Text>
-        <FlatList 
-          data={props}
-          renderItem={({item}) => <Friends name={item.name} hobby={item.hobby} />}
-        />
-      </View>
-    </TouchableOpacity>
-  );
-}
-
-const FriendsInfo = (props: any) => {
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        height: 100,
-        padding: 20
-      }}
-    >
-      <View style={{ backgroundColor: "blue", flex: 0.3 }} />
-      <View style={{ backgroundColor: "red", flex: 0.5 }} />
-      <Text>Hello World!</Text>
+    <View style={styles.item}>
+      <Text>{'\u2022'} Name: {props.name}, Hobby: {props.hobby} </Text>
     </View>
   );
-  console.log("FriendsInfo");
 }
 
 function App() {
   const [DATA, setData] = useState();
   return (
     <View style={styles.container}>
+      <ScrollView>
        <FlatList
         data={DATA}
-        renderItem={({item}) => <Friends name={item.name} hobby={item.hobby} />}
-        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => 
+          <TouchableOpacity onPress={() => {
+            alert( "Name: \t" +item.name+"\n"
+                  +"Hobby: \t"+item.hobby+"\n"
+                  +"Age: \t"  +item.age+"\n"
+                  +"Address: \t"+item.address+"\n"
+                  +"Phone: \t"+item.phone+"\n"
+            );
+            console.log(item.name);
+            console.log(item.hobby);
+            console.log(item.age);
+            console.log(item.address);
+            console.log(item.phone);
+          }}>
+            <Friends name={item.name} hobby={item.hobby} />
+          </TouchableOpacity>
+        }
+        keyExtractor={(item, index) => index.toString()
+        }
       />
+      
       <Button title="Load" onPress={() => {
           const raw =  fetch('https://raw.githubusercontent.com/AndresSV/Activity3.1/main/friends.json').then(response => response.json());
           raw.then( (data) => setData(data) );
         }
       }/>
       <StatusBar style="auto" />
+      </ScrollView>
     </View>
   );
 }
@@ -65,7 +59,6 @@ const styles = StyleSheet.create({
   item: {
     padding: 10,
     fontSize: 18,
-    height: 22,
   },
 });
 
